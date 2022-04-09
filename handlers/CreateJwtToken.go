cda3748917e6ebb4c2d0bfe47d2271b02c580c0f
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-04-09 00:42:35 +0000

    if incomingUser variable has any of its fields empty server will now send a bad request response

--- handlers/CreateJwtToken.go
+++ handlers/CreateJwtToken.go
@@ -32,37 +32,42 @@ func CreateJwtToken(writer http.ResponseWriter, req *http.Request, params httpro
 	if decodingErr != nil {
 		writer.WriteHeader(http.StatusInternalServerError)
 	}
-	// to create token we first need to createa UserClaims type instance,
-	// its fields will come primarly from UserRequest type instance
-	var claims UserClaims = UserClaims{
-		UserRequest: UserRequest{
-			Username: incomingUser.Username,
-			Email:    incomingUser.Email,
-			Name:     incomingUser.Name,
-		},
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Second * 60)),
-		},
+	if len(incomingUser.Username) <= 0 || len(incomingUser.Name) <= 0 || len(incomingUser.Email) <= 0 {
+		writer.WriteHeader(http.StatusBadRequest)
+	} else {
+		// to create token we first need to createa UserClaims type instance,
+		// its fields will come primarly from UserRequest type instance
+		var claims UserClaims = UserClaims{
+			UserRequest: UserRequest{
+				Username: incomingUser.Username,
+				Email:    incomingUser.Email,
+				Name:     incomingUser.Name,
+			},
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Second * 60)),
+			},
+		}
+
+		// we also need a jwt.SigningMethod type instance to create token
+		var signingMethod jwt.SigningMethod = jwt.GetSigningMethod(jwt.SigningMethodHS256.Alg())
+		// finally we can create a jwt.Token type instance with the signing method,
+		// and claims created above
+		token := jwt.NewWithClaims(signingMethod, claims)
+		// the final step is to sign the token with a key, in this case the
+		// key was created ouside this project, keygenerator.io is highly
+		// recommended
+		stringToken, tokenErr := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
+		if tokenErr != nil {
+			panic(tokenErr)
+		}
+
+		// now we send token via http request
+		var data map[string]string = make(map[string]string)
+		data["token"] = stringToken
+		jsonResponse, _ := json.Marshal(data)
+		writer.WriteHeader(http.StatusAccepted)
+		writer.Write(jsonResponse)
 	}
 
-	// we also need a jwt.SigningMethod type instance to create token
-	var signingMethod jwt.SigningMethod = jwt.GetSigningMethod(jwt.SigningMethodHS256.Alg())
-	// finally we can create a jwt.Token type instance with the signing method,
-	// and claims created above
-	token := jwt.NewWithClaims(signingMethod, claims)
-	// the final step is to sign the token with a key, in this case the
-	// key was created ouside this project, keygenerator.io is highly
-	// recommended
-	stringToken, tokenErr := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
-	if tokenErr != nil {
-		panic(tokenErr)
-	}
-
-	// now we send token via http request
-	var data map[string]string = make(map[string]string)
-	data["token"] = stringToken
-	jsonResponse, _ := json.Marshal(data)
-	writer.WriteHeader(http.StatusAccepted)
-	writer.Write(jsonResponse)
 	defer req.Body.Close()
 }
