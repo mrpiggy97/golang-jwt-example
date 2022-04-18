6f2573bbbf56340dee6af72d3829d4dd480835ab
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-04-18 16:49:40 +0000

    added new endpoint

--- server/GetServer.go
+++ server/GetServer.go
@@ -10,6 +10,7 @@ import (
 func GetServer() http.Server {
 	var router *httprouter.Router = httprouter.New()
 	router.POST("/api/auth/token", handlers.CreateJwtToken)
+	router.GET("/api/auth/verify", handlers.VerifyToken)
 	var address string = "0.0.0.0:8000"
 	return http.Server{Addr: address, Handler: router}
 }
